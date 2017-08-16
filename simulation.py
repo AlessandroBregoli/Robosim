6973ccc5dc1d5edace3545409dec6b4f01b01b53
alessandrobregoli <alessandroxciv@gmailc.om>
2017-08-16 18:14:30 +0200

    Aggiunto esempio

--- simulation.py
+++ simulation.py
@@ -2,3 +2,10 @@ import model
 
 mappa = model.load_map("mappa_1.txt")
 modello = model.Robosim_model(2, mappa)
+print("Mappa da esplorare:")
+model.print_map(mappa)
+for tmp in range(5):
+    print("20 iterazioni:")
+    for x in range(20):
+        modello.step()
+    model.print_map(modello.explored_map)
